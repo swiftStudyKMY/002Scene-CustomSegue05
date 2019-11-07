d0f89f511a611fe4a171ae1a893bdf99f0dfcdb8
MinyoungKim <801sanae@gmail.com>
2019-11-07 22:08:33 +0900

    Initial Commit

---
+++ 002Scene-CustomSegue05/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  002Scene-CustomSegue05
+//
+//  Created by 김민영 on 11/7/19.
+//  Copyright © 2019 김민영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
